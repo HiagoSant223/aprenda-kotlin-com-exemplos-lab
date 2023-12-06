29a91a6aa217c43774e0e663fcaba665b69d89f3
HiagoSant223 <105986893+HiagoSant223@users.noreply.github.com>
2023-12-06 16:50:47 -0300

    Projeto final

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,40 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(var nome: String, var Nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} está matriculado na formacao ${nome}")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val usuario1 = Usuario("Hiago")
+    val usuario2 = Usuario("Felipe")
+    val usuario3 = Usuario("Mariana")
+    val usuario4 = Usuario("Luiz")
+    val usuario5 = Usuario("Marcos")
+
+    val conteudo1 = ConteudoEducacional("Formação Java", 60)
+    val conteudo2 = ConteudoEducacional("Formação Kotlin", 160)
+    val conteudo3 = ConteudoEducacional("Formação JavaScript", 70)
+    val conteudo4 = ConteudoEducacional("Formação Angular", 20)
+    val conteudo5 = ConteudoEducacional("Formação Pyton", 110)
+
+    val formacao = Formacao("Desenvolvedor Full-stack", Nivel.AVANCADO, listOf(conteudo1, conteudo3, conteudo4, conteudo2, conteudo5 ))
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario3)
+    formacao.matricular(usuario4)
+    formacao.matricular(usuario5)
+    
 }
+    
